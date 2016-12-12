b8d9107eb57e64de92c3138ac6dd55e2966f0f98
XueFengZeng <xuefeng.zeng@hypereal.com>
2016-12-12 16:49:40 +0800

    Finished 'One-vs-All Classifier Prediction'.

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -35,16 +35,10 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-
-
-
-
-
-
-
-
-
-
+h=sigmoid(X*theta);
+J=1/m*(-y'*log(h)-(1-y)'*log(1-h))+lambda/(2*m)*(theta(2:end)'*theta(2:end));
+grad=1/m*X'*(h-y);
+grad(2:end)=grad(2:size(theta))+lambda/m*theta(2:end);
 % =============================================================
 
 grad = grad(:);
--- ex3/predictOneVsAll.m
+++ ex3/predictOneVsAll.m
@@ -29,14 +29,7 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
-
-
-
-
-
+h=sigmoid(X*all_theta');
+[mv,p]=max(h,[],2);
 % =========================================================================
-
-
 end
